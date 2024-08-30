d7316bffea63f4a9b2012d656837f9904f720013
Victoria <shevvictoryia@gmail.com>
2024-08-30 22:45:49 +0300

    feat: add keycode functionality

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const wrapper = document.querySelector(".wrapper");
 window.onkeydown = (event) => {
-  wrapper.appendChild(`
+  wrapper.innerHTML = `
     <div class="container">
-        <p></p>
-        <div class="container__key-descr">Press any key to start</div>
+        <p class="container__title">event.key</p>
+        <div class="container__key-descr">${
+          event.key === " " ? "space" : event.key
+        }</div>
       </div>
-    `);
+      <div class="container">
+        <p class="container__title">event.keyCode</p>
+        <div class="container__key-descr">${event.keyCode}</div>
+      </div>
+      <div class="container">
+        <p class="container__title">event.code</p>
+        <div class="container__key-descr">${event.code}</div>
+      </div>
+    `;
 };
